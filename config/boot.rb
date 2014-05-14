1b12eee6cb0d1dcba1f346daa09511863cbfd8cc
klebervirgilio <klebervirgilio@gmail.com>
2014-05-14 17:59:12 -0300

    Pry as default REPL

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,5 @@
 ENV["RACK_ENV"] ||= "development"
 
-require "pry" if ENV["RACK_ENV"] != "production"
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"].to_sym)
@@ -13,6 +12,7 @@ REDIS  = if ENV["RACK_ENV"] != "production"
           Redis.new(url: ENV['REDISTOGO_URL'])
         end
 
+$:.concat Dir['./lib/**/']
 Dir["./lib/**/*.rb"].each{|f| require f }
 
 Mongoid.load!(File.expand_path("../../config/mongoid.yml", __FILE__))
